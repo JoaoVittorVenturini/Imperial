3db34f97f3de1b16c638a546145d7f232d1926fd
JJAAX <106125078+JJAAX@users.noreply.github.com>
2022-11-22 16:34:16 -0300

    Create Actions.php

---
+++ cargo/Actions.php
@@ -0,0 +1,62 @@
+<?php
+  include "conecta.php";
+  if(isset($_REQUEST['acao'])){
+  switch($_REQUEST["acao"]){
+    case 'cadastrar':
+      $id = $_POST["id"];
+      $cargo = $_POST["nome"];
+      $descricao = $_POST["descricao"];
+      $salario = $_POST["salario"];
+      $cargahoraria = $_POST["carga"];
+      
+        $sql = "INSERT INTO usuario (id, nome, descricao, salario, cargaHoraria) VALUES ('{$id}', '{$cargo}', '{$descricao}', '{$salario}', '{$cargahoraria}')";
+
+        $res = $conn->query($sql);
+
+        if($res==true){
+          print "<script>alert('Cargo cadastrado com sucesso!')</script>";
+          print "<script>location.href = listaCargo.php'</script>";
+        }else{
+          echo "<script>alert('Erro ao tentar cadastrar!')</script>";
+        };
+    break;
+    
+    case 'editar':
+      $id           = $_POST["id"];
+      $cargo          = $_POST["nome"];
+      $descricao         = $_POST["descricao"];
+      $salario       = $_POST["salario"];
+      $cargahoraria            = $_POST["carga"];
+
+      $sql = "UPDATE cargo SET
+      nome = '{$cargo}',
+      descricao = '{$descricao}',
+      salario = '{$salario}',
+      cargaHoraria = '{$cargahoraria}'
+      WHERE id = '{$id}'";
+
+      $res = $conn->query($sql);
+
+      if($res==true){
+          print "<script>alert('Cargo editado com sucesso!')</script>";
+      }else{
+          echo "<script>alert('Não foi possivel editar o cargo!')</script>";
+      };
+      print "<script>location.href = 'listaCargo.php'</script>";
+      break;
+
+    case 'excluir':
+      $sql = "DELETE FROM usuario WHERE id = ".$_REQUEST['id'];
+
+      $res = $conn->query($sql);
+
+      if($res==true){
+        print "<script>alert('Usuário excluido com sucesso!') </script>";
+        print "<script>location.href = 'listaCargo.php' </script>";
+      }else{
+        echo "<script>alert('Erro ao tentar excluir usuário!') </script>";
+      };
+    break;
+  }
+}
+?>
