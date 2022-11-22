2743ff5695f579ec59f00986c3ade89280c6d029
JJAAX <106125078+JJAAX@users.noreply.github.com>
2022-11-22 16:50:55 -0300

    Create excluir.php

---
+++ Cargo/excluir.php
@@ -0,0 +1,19 @@
+<?php
+  include_once("conecta.php");
+
+  if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM cargo WHERE id = ".$_REQUEST['id'];
+
+    $res = $conn->query($sql);
+
+    if($res==true){
+      print "<script>alert('Cargo deletado com sucesso!')</script>";
+
+    }else{
+      echo "<script>alert('Erro ao tentar deletar cargo!')</script>";
+    };
+    print "<script>location.href='listaCargo.php'</script>";
+  }
+?>
