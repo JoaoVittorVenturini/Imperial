0a5421953e798e29e89c4749290dbd9f546cc6cc
Morghhul <112036862+Morghhul@users.noreply.github.com>
2022-11-22 23:38:26 -0300

    Update Update.php

--- Usuarios/Update.php
+++ Usuarios/Update.php
@@ -1,5 +1,5 @@
 <?php
-    include "connectDB.php";
+    include "../db/connectDB.php";
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
     <title>Editar Produto</title>
@@ -17,8 +17,8 @@
 
 <body>
     <header>
-        <a href="">
-            <img src="Logo.svg" alt="Logo do time Imperial eSports">
+        <a href="../index.html">
+            <img src="../img/Logo.svg" alt="Logo do time Imperial eSports">
         </a>
     </header>
     
@@ -36,7 +36,7 @@
     <main>
         <div class="conteudo medio text-center">
             <h1 class="titulo">Editar Usuario</h1>
-            <form action="Actions.php" method="post">
+            <form action="./Actions.php" method="post">
                 <input type="hidden" name="acao" value="editar">
                 <div class="row">
                     <div class="col">
@@ -79,5 +79,4 @@
         ?>
     </footer>
 </body>
-
 </html>
