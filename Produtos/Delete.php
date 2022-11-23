189d25773d1a14a6c93ae7c60520f963b6e9ae14
Nathan Ferreira <107155373+Nzzinn@users.noreply.github.com>
2022-11-22 23:45:19 -0300

    Add files via upload

---
+++ Produtos/Delete.php
@@ -0,0 +1,17 @@
+<?php
+  include "connectDB.php";
+
+  if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "DELETE FROM produtos WHERE id = ".$id;
+
+    $res = $conn->query($sql);
+
+    if($res==true){
+      print "<script>alert('Produto excluido com sucesso!')</script>";
+    }else{
+      print "<script>alert('OPS!!! Algo deu errado.')</script>";
+    };
+  }
+  print "<script>location.href = 'Read.php'</script>";
+?>
