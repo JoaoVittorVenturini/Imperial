0761b010acd4499150f188b376a49a0b8f5a32f5
Morghhul <112036862+Morghhul@users.noreply.github.com>
2022-11-22 23:40:31 -0300

    Update Delete.php

--- Produtos/Delete.php
+++ Produtos/Delete.php
@@ -1,16 +1,16 @@
 <?php
-  include_once("../db/connectDB.php");
+  include "../db/connectDB.php";
 
   if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $sql = "DELETE FROM produtos WHERE id = ".$id;
+    $sql = "DELETE FROM usuario WHERE id = ".$id;
 
     $res = $conn->query($sql);
 
     if($res==true){
-      print "<script>alert('Produto excluido com sucesso!')</script>";
+      print "<script>alert('Usuário excluido com sucesso!')</script>";
     }else{
-      print "<script>alert('OPS!!! Algo deu errado.')</script>";
+      print "<script>alert('Usuário não pode ser excluido.')</script>";
     };
   }
   print "<script>location.href = './Read.php'</script>";
